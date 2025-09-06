78e9614b3ca1f75fab00dba51ef746dbb010f18f
Munehito Kanno <munehito.kanno@gmail.com>
2025-09-06 23:35:46 +0900

    missavになかったらav01で検索する

---
+++ a/browserExtensions/searchOnAV01FromMissav/background.js
@@ -0,0 +1,22 @@
+// 拡張機能のインストール時にメニューを作成
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "search-av01-from-missav",
+    title: "pageURLをAV01で、さがす",
+    contexts: ["page"],
+    documentUrlPatterns: ["https://missav.ai/*"],
+    icons: {
+      "16": "icons/av01_16.png"
+    }
+  });
+});
+
+// メニューがクリックされた時の処理
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  let searchResultsUrl = "https://www.av01.tv/jp/search?q=" + info.pageUrl.split('/').filter(Boolean).pop().replace(/-/g, '+') + "&s=seeders&o=desc"
+  chrome.tabs.create({
+    openerTabId: tab.id,
+    url: searchResultsUrl,
+    active: true
+  });
+});
